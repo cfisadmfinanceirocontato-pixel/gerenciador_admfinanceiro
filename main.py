aaeecdf3b9af1277dbb5e4e371a3792081376fb2
cfisadmfinanceirocontato-pixel <cfis.admfinanceiro.contato@gmail.com>
2026-02-05 01:50:50 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# Importar as bibliotecas
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+
+# Criar as funções de carregamento de dados
+
+#preparar as visualizações
+
+#criar a interface do streamlit
+st.write("""
+# Análise de preços de ações
+O gráfico abaixo representa a evolução do preço de uma acão ao longo do tempo         
+""") # Markdown
