263bf97f5d744f5cf3c6f1e0a95e4564c412f42a
cfisadmfinanceirocontato-pixel <cfis.admfinanceiro.contato@gmail.com>
2026-02-11 03:02:19 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.set_page_config(page_title="Cfis App Financeiro")
 
-st.markdown("""
-# Boas-vindas ao Cfis App Financeiro!
+pg = st.navigation([
+    #st.markdown("# Cfis App Financeiro"), 
+    st.Page("pages/home.py", title="Home"),
+    st.Page("pages/usuarios.py", title="Usuários"),
+    st.Page("pages/pginstrumentos.py", title="Demonstrativo financeiro"),
+    st.Page("pages/pgprovisionamento.py", title="Provisionamento mensal"),
+    st.Page("pages/pgdiarias.py", title="Controle de Diárias"),
+    st.Page("pages/pgpessoal.py", title="Pagamento de pessoal"),
+    st.Page("pages/pgconsumo.py", title="Pagamento de consumo"),
+    st.Page("pages/pgservicos.py", title="Pagamento de serviços"),
+    st.Page("pages/pgfuncionarios.py", title="Cadastro de Funcionários"),
+    st.Page("pages/pgfornecedores.py", title="Cadastro de Fornecedores"),
+])
 
-## O app foi desenvolvido para auxiliar no controle financeiro.
-            
-""")
-
-st.sidebar.header("🏤 Cfis App Financeiro")
+pg.run()
 
 
-file_uplpad = st.file_uploader("Faça o upload do seu arquivo CSV para começar a usar o app.", type=["csv"])
-if file_uplpad is not None:
-    df = pd.read_csv(file_uplpad)
-    st.dataframe(df, hide_index=True)
+
 
