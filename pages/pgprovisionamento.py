5363c430fa6f4cadf4677d9488ea9b7a50b9e25a
cfisadmfinanceirocontato-pixel <cfis.admfinanceiro.contato@gmail.com>
2026-02-12 03:49:37 -0300

    Add files via upload

--- pages/pgprovisionamento.py
+++ pages/pgprovisionamento.py
@@ -1,13 +1,23 @@
+# --- IMportando as bibliotecas ---
 import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Esta é a minha página de provisionamento mensal de pagamento de NFE's!")
+# --- Configurações da página ---
+st.set_page_config(page_title="Dados gerais!", layout="wide")
+
+# --- Título da página ---
+st.title("Provisionamento mensal custo direto.")
+
+
+# --- Pldanilha de dados gerais ----
+
+df_custoDireto = pd.read_csv("provisionamento_mensal_cd.csv", low_memory=True)
+st.write(df_custoDireto)
+
 
 file_uplpad = st.file_uploader("Faça o upload do seu arquivo CSV para começar a usar o app.", type=["csv"])
 if file_uplpad is not None:
     df = pd.read_csv(file_uplpad)
     st.dataframe(df, hide_index=True)
-
-
 
