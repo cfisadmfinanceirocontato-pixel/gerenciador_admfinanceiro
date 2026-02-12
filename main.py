d4a9568a8c2bf0d5d492054781dfd0a280ed68ec
cfisadmfinanceirocontato-pixel <cfis.admfinanceiro.contato@gmail.com>
2026-02-12 00:42:24 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ st.set_page_config(page_title="Cfis App Financeiro")
 
 pg = st.navigation([
     #st.markdown("# Cfis App Financeiro"), 
-    st.Page("home.py", title="Home"),
-    st.Page("usuarios.py", title="Usuários"),
-    st.Page("pginstrumentos.py", title="Demonstrativo financeiro"),
-    st.Page("pgprovisionamento.py", title="Provisionamento mensal"),
-    st.Page("pgdiarias.py", title="Controle de Diárias"),
-    st.Page("pgpessoal.py", title="Pagamento de pessoal"),
-    st.Page("pgconsumo.py", title="Pagamento de consumo"),
-    st.Page("pgservicos.py", title="Pagamento de serviços"),
-    st.Page("pgfuncionarios.py", title="Cadastro de Funcionários"),
-    st.Page("pgfornecedores.py", title="Cadastro de Fornecedores"),
+    st.Page("pages/home.py", title="Home"),
+    st.Page("pages/usuarios.py", title="Usuários"),
+    st.Page("pages/pginstrumentos.py", title="Demonstrativo financeiro"),
+    st.Page("pages/pgprovisionamento.py", title="Provisionamento mensal"),
+    st.Page("pages/pgdiarias.py", title="Controle de Diárias"),
+    st.Page("pages/pgpessoal.py", title="Pagamento de pessoal"),
+    st.Page("pages/pgconsumo.py", title="Pagamento de consumo"),
+    st.Page("pages/pgservicos.py", title="Pagamento de serviços"),
+    st.Page("pages/pgfuncionarios.py", title="Cadastro de Funcionários"),
+    st.Page("pages/pgfornecedores.py", title="Cadastro de Fornecedores"),
 ])
 
 pg.run()
